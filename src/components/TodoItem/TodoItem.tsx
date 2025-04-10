9640d7630ddc86556eb8545883e30772743ed2ea
NikaL25 <mr.lomiashvil@mail.ru>
2025-04-10 13:12:32 +0400

    changed checkmark svg

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { toggleTodo } from '../../store/reducers/todoReducer'
 import './TodoItemStyle.scss'
+
 import unchecked from '../../svg/radio_unchecked.svg'
-import checked from '../../svg/radio_checked.svg'
+import greenCheck from '../../svg/checkmark-svgrepo-com.svg' 
 
 interface ITodoItemProps {
   id: string,
@@ -13,22 +14,19 @@ interface ITodoItemProps {
 
 function TodoItem({ id, text, isActive }: ITodoItemProps) {
   const dispatch = useDispatch()
+
   return (
-    <>
-      <div className="todo-item">
-        <img
-          src={isActive ? unchecked : checked}
-          alt=""
-          className="todo-item__radio"
-          onClick={() => {
-            dispatch(toggleTodo(id));
-          }}
-        />
-        <span className={ isActive ? "todo-item__text" : "todo-item__text finished"}>
-          {text}
-        </span>
-      </div>
-    </>
+    <div className="todo-item">
+      <img
+        src={isActive ? unchecked : greenCheck}
+        alt="toggle"
+        className="todo-item__icon"
+        onClick={() => dispatch(toggleTodo(id))}
+      />
+      <span className={isActive ? "todo-item__text" : "todo-item__text finished"}>
+        {text}
+      </span>
+    </div>
   )
 }
 
